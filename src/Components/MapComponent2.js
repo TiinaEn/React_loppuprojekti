d958605f695b2a476adb0755dc5dcd85d11c91df
TiinaEn <tiina.enroth@outlook.com>
2018-04-05 15:57:34 +0300

    hakusana --> submit --> kartalle tulee marker

--- src/Components/MapComponent2.js
+++ src/Components/MapComponent2.js
@@ -7,6 +7,12 @@ import input from "eslint-plugin-jsx-a11y/src/util/implicitRoles/input";
 
 
 class MapComponent2 extends Component {
+
+   /* state = {
+        locations: [
+            {name: "New York County Supreme Court", location: {lat: 40.7143033, lng: -74.0036919} }
+        ]
+    }*/
     componentDidMount() {
         this.loadMap();
     }
@@ -23,40 +29,16 @@ class MapComponent2 extends Component {
                 mapTypeId: 'roadmap'
             })
 
+
             var map = new maps.Map(node, mapConfig);        //presenting a map on our site
             var marker;
 
-            var autocomplete = new google.maps.places.Autocomplete(input);
-            autocomplete.bindTo('bounds', map);
-
-            /*var info = new google.maps.InfoWindow({
-                content: 'Hei'
-            }) ;*/
-
-            var place = autocomplete.getPlace();
-            if (!place.geometry){
-                window.alert("No details available");
-                return;
-            }
-            if (place.geometry.viewport){
-                map.fitBounds(place.geometry.viewport);
-            } else {
-                map.setCenter(place.geometry.location);
-                map.setZoom(17);
-            }
-            marker.setPosition(place.geometry.location);
-            marker.setVisible(true);
-
-/*            var autocomplete = new google.maps.places.Autocomplete();
-            var place = autocomplete.getPlace;
-            autocomplete.bindTo('bound', map);
-            if (place.geometry.viewport) {
-                map.fitbounds(place.geometry.viewport);
-            }else {
-                map.setCenter(place.geometry.location);
-                map.setZoom(17);*/
-          /*  }*/
-
+/*            this.state.locations.forEach((location =>
+            marker = new google.maps.Marker({
+                position: {getLatLng},
+                map: map,
+                title: ''
+            })*/
 
             map.addListener('click', function(event) {      //function for adding a marker on a map
                 placeMarker(event.latLng, map);
@@ -68,6 +50,8 @@ class MapComponent2 extends Component {
                     map: map
                 });
                 map.panTo(latLng);
+
+
 
                 google.maps.event.addListener(marker, 'click', function() {
                    infoWindow.open(map, marker);    //presenting an infowindow when double clicking the marker
@@ -93,15 +77,11 @@ class MapComponent2 extends Component {
 
             /*var messageWindow = new google.maps.InfoWindow({
                content: '<div>Location saved</div>'
-
             });
-
-
-
             });*/
+        }
+        }
 
-        }
-        }
 
 
     constructor(props) {
@@ -110,18 +90,47 @@ class MapComponent2 extends Component {
         this.onChange = (address) => this.setState({ address })
     }
 
-    handleFormSubmit = (event) => {
+   /* var latlong;*/
+   /* handleFormSubmit = (event) => {
         event.preventDefault()
 
         geocodeByAddress(this.state.address)
-        .then(results => getLatLng(results[0]))
-            .then(latLng => console.log('Success', latLng))
+            .then(results => getLatLng(results[0]))
+
+            .then(latLng => console.log('success', /!*latlong = *!/latLng))
             .catch(error => console.error('Error', error))
+    }*/
+    handleFormSubmit = (event) => {
+        event.preventDefault()
+        const {google} = this.props;
+        const maps = google.maps;
+        const mapRef = this.refs.map;
+        const node = ReactDOM.findDOMNode(mapRef);
+        const mapConfig = Object.assign({}, {
+            center: {lat: 60, lng: 25},
+            zoom: 5,
+            mapTypeId: 'roadmap'
+        })
+        var map = new maps.Map(node, mapConfig);
+        geocodeByAddress(this.state.address)
+            .then(results => getLatLng(results[0]))
+            .then(latLng => new google.maps.Marker({
+                position: latLng,
+                map: map}))
+
+    .catch(error => console.error('Error', error))
     }
 
 
+/*    var sijainti = new google.maps.Marker({
+        position: {lat: latlong.lat, lng: latlong.lng}, // sets position of marker to specified location
+        map: map, // sets markers to appear on the map we just created on line 35
+        title: "" // the title of the marker is set to the name of the location
+    });
+    sijainti.addListener('click', handleFormSubmit);*/
 
     render() {
+
         const style = {
             width: '100vw',
             height: '50vh'
@@ -132,7 +141,6 @@ class MapComponent2 extends Component {
             /*type: 'search',*/
             placeholder: 'Search places'
         }
-
 
         return (
             <div>
