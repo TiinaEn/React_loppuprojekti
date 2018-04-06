617191f00b12d2d661f729ec344c5b43320917b8
MillaPeltola <milla.peltola@icloud.com>
2018-04-06 13:40:59 +0300

    CreateEntry-formia päivitetty

--- src/Components/CreateEntry.js
+++ src/Components/CreateEntry.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import '../App.css';
+import '../Note.css';
 import MapComponent2 from "./MapComponent2";
 
 class CreateEntry extends Component {
@@ -17,10 +18,11 @@ class CreateEntry extends Component {
     render() {
         return (
             <div className="xx">
+                <h1>New Note</h1>
+                <br/>
                 <form onSubmit={this.handleSubmit}>
-
-                    <label>Title: </label>
-                    <input type="text" value={this.state.name} onChange={this.handleChange}/>
+                    <label>Name: </label>
+                    <textarea type="text" placeholder="Name" value={this.state.name} onChange={this.handleChange}/>
                     <br/>
                     <label>Category: </label>
                     <select>
@@ -30,11 +32,16 @@ class CreateEntry extends Component {
                         <option value=''>Sights</option>
                         <option value=''>Other</option>
                     </select>
+                    <br/>
+                    <label>Address: </label>
+                    <textarea type="text" placeholder="Address" value={this.state.address} onChange={this.handleChange}/>
+                    <br/>
                     <label>Description: </label>
-                    <textarea type="text" placeholder="Muistiinpanon kuvaus" value={this.state.description} onChange={this.handleChange}/>
-
-                    <input type="submit" value="Luo" onClick={this.handleCreateClick}/>
+                    <textarea type="text" placeholder="Description" value={this.state.description} onChange={this.handleChange}/>
+                    <br/>
+                    <input type="submit" value="Save" onClick={this.handleCreateClick}/>
                 </form>
+                <br/>
                 <MapComponent2/>
             </div>
         );
