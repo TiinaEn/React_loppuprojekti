d3f11fd007017de873763d58eeca27a838f19e8a
MillaPeltola <milla.peltola@icloud.com>
2018-04-06 11:31:40 +0300

    browse fetch

--- src/Components/Browse.js
+++ src/Components/Browse.js
@@ -3,13 +3,28 @@ import React, { Component } from 'react';
 import '../App.css';
 
 class Browse extends Component {
+//     state = {destinations: []};
+//
+//     componentDidMount() {
+//         fetch('/travelapp/destinations')
+//             .then(function (response) {
+//                 return response.json();
+//             })
+//             .then(function (json) {
+//                 this.setState({destinations: json});
+//             }.bind(this));
+//
+//     }
+//
     render() {
+//         var destination = this.state.destinations.map(function (destination) {
+//             return <p key={destination.id}>{destination.country} {destination.city}</p>
+//         });
         return (
             <div className="Browse">
 
                 <h1>Destinations</h1>
-                <div>Lista tallennettuja maita, joiden jokaisen alla lista kyseisen maan tallennetuista kaupungeista</div>
-
+               {/*{destination}*/}
             </div>
         );
     }
