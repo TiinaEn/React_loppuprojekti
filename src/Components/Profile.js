45c84ad080db0c8ad0fe013d2b046c77642f0640
MillaPeltola <milla.peltola@icloud.com>
2018-04-06 11:16:22 +0300

    sivun päivitystä

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 
 import '../App.css';
+import '../Profile.css';
+import userimg from '../userimg.png'
+
 
 class Profile extends Component {
     render() {
         return (
             <div className="Profile">
 
-                <h1>Profile</h1>
+                <h1>My Profile</h1>
+                <br/>
+                <img style={{width: '20%',}} src={userimg}/>
                 {/*<div>Username</div>*/}
                 {/*<form action="">Email</form>*/}
                 {/*<form action="">Password</form>*/}
