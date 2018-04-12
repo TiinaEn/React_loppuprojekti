0542e580eb8438beb9e26f4a194010e15794e622
aimipe <35955223+aimipe@users.noreply.github.com>
2018-04-12 20:32:09 +0300

    formin pikasiistiminen

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -84,15 +84,15 @@ class Login extends Component {
 
                 <form class="form-horizontal" onSubmit={this.handleSubmit}>
                     <div className="form-group" value={this.state.usernameOrEmail} onChange={this.handleUsernameChange}>
-                        <label htmlFor="usernameOrEmail" class="control-label col-sm-4">Username</label>
-                        <div class="col-sm-4">
+                        <label htmlFor="usernameOrEmail" class="control-label col-sm-5">Username</label>
+                        <div class="col-sm-3">
                             <input type="text" className="form-control"/>
                         </div>
                     </div>
 
                     <div className="form-group" value={this.state.password} onChange={this.handlePasswordChange}>
-                        <label htmlFor="usernameOrEmail" class="control-label col-sm-4">Password</label>
-                            <div class="col-sm-4">
+                        <label htmlFor="usernameOrEmail" class="control-label col-sm-5">Password</label>
+                            <div class="col-sm-3">
                                 <input type="password" className="form-control"/>
                             </div>
                         </div>
