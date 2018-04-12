a75c09f0aa542cbc8a2f70b03cfe67a36b4013e2
MillaPeltola <milla.peltola@icloud.com>
2018-04-12 13:29:24 +0300

    Profiilista poistettu kovakoodatut arvot

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {render, Simulate, wait} from 'react-testing-library'
+import 'dom-testing-library/extend-expect'
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
